23a05b10c252be8fd7a054f5eefa531990dbb2cb
Nazar Pylypiak <pylypyaknazar@gmail.com>
2025-11-06 11:59:01 +0200

    refactor(layout): relocate top-bar to app.component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TopBarComponent } from '@shared/containers/top-bar/top-bar.component';
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, TopBarComponent],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
